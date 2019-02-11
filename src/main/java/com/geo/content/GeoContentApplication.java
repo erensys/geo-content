07c7b08b6af086ecb02dfa19388d1d265f07ca30
shishirapradhan2015@gmail.com <shishirapradhan2015@gmail.com>
2019-02-11 10:24:56 +0530

    fitst comment

---
+++ src/main/java/com/geo/content/GeoContentApplication.java
@@ -0,0 +1,14 @@
+package com.geo.content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoContentApplication.class, args);
+	}
+
+}
+
