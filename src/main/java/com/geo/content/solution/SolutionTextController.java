f1c48bb3483fe5d81aea5cec2ce85354a6274a62
shishirapradhan2015@gmail.com <shishirapradhan2015@gmail.com>
2019-02-11 10:48:41 +0530

    solution text added

---
+++ src/main/java/com/geo/content/solution/SolutionTextController.java
@@ -0,0 +1,14 @@
+package com.geo.content.solution;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/solution")
+public class SolutionTextController {
+	
+	@GetMapping("text")
+	public String getSolutionText() {
+		return "Solution Text";
+	}
+
+}
