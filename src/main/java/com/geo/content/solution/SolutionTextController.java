cb41aea073ab883d722e14156d68550ab37e8c21
shishirapradhan2015@gmail.com <shishirapradhan2015@gmail.com>
2019-02-11 13:03:38 +0530

    add return type

--- src/main/java/com/geo/content/solution/SolutionTextController.java
+++ src/main/java/com/geo/content/solution/SolutionTextController.java
@@ -1,14 +1,20 @@
 package com.geo.content.solution;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/solution")
 public class SolutionTextController {
 	
-	@GetMapping("text")
+	
+	private static final Logger LOG = LoggerFactory.getLogger(SolutionTextController.class);
+	
+	@GetMapping("/text")
 	public String getSolutionText() {
-		return "Solution Text";
+		LOG.debug("SolutionTextController - getSolutionText()");
+		return new String("Test");
 	}
 
 }
